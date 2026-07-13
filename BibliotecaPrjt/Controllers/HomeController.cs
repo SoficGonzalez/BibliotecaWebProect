908ea32aa8524160793830859a33c5863c8acce3
SoficGonzalez <2025020179@cvirtualuees.edu.sv>
2026-07-13 15:47:26 -0600

    Ejercicios Practicos Semana 1

--- BibliotecaPrjt/Controllers/HomeController.cs
+++ BibliotecaPrjt/Controllers/HomeController.cs
@@ -26,6 +26,21 @@ namespace BibliotecaPrjt.Controllers
             return View();
         }
 
+        public IActionResult Usuario()
+        {
+            return View();
+        }
+
+        public IActionResult Prestamo()
+        {
+            return View();
+        }
+
+        public IActionResult AcercaDe()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- BibliotecaPrjt/obj/Debug/net10.0/BibliotecaPrjt.AssemblyInfo.cs
+++ BibliotecaPrjt/obj/Debug/net10.0/BibliotecaPrjt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaPrjt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a7e5b97370e2b8ebbea095ed46aab1eafc971e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaPrjt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotecaPrjt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

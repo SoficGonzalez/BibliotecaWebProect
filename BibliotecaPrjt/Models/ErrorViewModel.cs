0a7e5b97370e2b8ebbea095ed46aab1eafc971e9
SoficGonzalez <2025020179@cvirtualuees.edu.sv>
2026-07-13 15:07:03 -0600

    Ejercicio realizado en clase

---
+++ BibliotecaPrjt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BibliotecaPrjt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

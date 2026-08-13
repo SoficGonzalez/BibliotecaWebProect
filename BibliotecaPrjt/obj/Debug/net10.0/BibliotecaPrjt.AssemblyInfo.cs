18e06f2e6d4e9abe4f14eebc21bd1fbf45d4d095
SoficGonzalez <2025020179@cvirtualuees.edu.sv>
2026-08-13 07:45:56 -0600

    Primera parte de la tarea sem 5

--- BibliotecaPrjt/obj/Debug/net10.0/BibliotecaPrjt.AssemblyInfo.cs
+++ BibliotecaPrjt/obj/Debug/net10.0/BibliotecaPrjt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaPrjt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+058a651f7c61bdcce70174be9f664afb9584cb37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7990614ebb7e9913ef99f5c17451cbf9338fa9dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaPrjt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotecaPrjt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

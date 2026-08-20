e0ef849a4eb6ca370c019da2819ea666d3d3c6b1
SoficGonzalez <2025020179@cvirtualuees.edu.sv>
2026-08-20 10:47:28 -0600

    Ejercicios Prácticos Semana 5

---
+++ BibliotecaPrjt/Models/FeatureItem.cs
@@ -0,0 +1,8 @@
+﻿namespace BibliotecaPrjt.Models
+{
+    public class FeatureItem
+    {
+        public string Header { get; set; }
+        public string Paragraph { get; set; }
+    }
+}
---
+++ BibliotecaPrjt/Models/ServiceCard.cs
@@ -0,0 +1,8 @@
+﻿namespace BibliotecaPrjt.Models
+{
+    public class ServiceCard
+    {
+        public string ImageUrl { get; set; }
+        public string Text { get; set; }
+    }
+}
